95c531c75a32fa1e0bb072a8ff23e1f0dec954e2
ech <pnam2311@gmail.com>
2025-03-30 16:20:50 +0700

    pinning kernel

--- system/core/boot.nix
+++ system/core/boot.nix
@@ -9,7 +9,18 @@
     };
 
     ## Kernel
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackagesFor (
+      pkgs.linux_6_12.override {
+        argsOverride = rec {
+          src = pkgs.fetchurl {
+            url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
+            sha256 = "15xjjn8ff7g9q0ljr2g8k098ppxnpvxlgv22rdrplls8sxg6wlaa";
+          };
+          version = "6.12.10";
+          modDirVersion = "6.12.10";
+        };
+      }
+    );
     initrd.kernelModules = [
       "amdgpu"
       "nvidia"
