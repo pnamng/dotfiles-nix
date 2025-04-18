ae72b1cc75fb1a617f050778e449c208c235fa2a
ech <pnam2311@gmail.com>
2025-04-18 18:07:40 +0700

    refactor home manager

--- home/default.nix
+++ home/default.nix
@@ -14,7 +14,6 @@
 
   # themes --------------------
   gtk = {
-
     enable = true;
     cursorTheme = {
       name = "Bibata-Modern-Ice";
@@ -56,54 +55,63 @@
       ghostty
     ];
 
-  programs.git = {
-    enable = true;
-    userName = "ech";
-    userEmail = "pnam2311@gmail.com";
-  };
+  programs = {
+    git = {
+      enable = true;
+      userName = "ech";
+      userEmail = "pnam2311@gmail.com";
+      extraConfig = {
+        core = {
+          editor = "nvim";
+        };
+      };
+    };
 
-  programs.neovim = {
-    enable = true;
-    package = pkgs-unstable.neovim-unwrapped;
-    extraLuaConfig = lib.fileContents ./nvim/init.lua;
-    extraPackages = with pkgs; [
-      vue-language-server
-      lua-language-server
-      typescript-language-server
-      pyright
-      nixd
-      nixfmt-rfc-style
-    ];
+    neovim = {
+      enable = true;
+      package = pkgs-unstable.neovim-unwrapped;
+      extraLuaConfig = lib.fileContents ./nvim/init.lua;
+      extraPackages = with pkgs; [
+        vue-language-server
+        lua-language-server
+        typescript-language-server
+        pyright
+        nixd
+        nixfmt-rfc-style
+      ];
+    };
+
+    home-manager.enable = true;
   };
 
   home = {
-    file.".config/hypr/" = {
-      source = ./hypr;
-      recursive = false;
-    };
+    file = {
+      ".config/hypr/" = {
+        source = ./hypr;
+        recursive = false;
+      };
 
-    file.".config/waybar/" = {
-      source = ./waybar;
-      recursive = false;
-    };
+      ".config/waybar/" = {
+        source = ./waybar;
+        recursive = false;
+      };
 
-    file.".tmux.conf".source = ./.tmux.conf;
-    file.".config/fuzzel/".source = ./fuzzel;
-    file.".config/alacritty/".source = ./alacritty;
-    file.".config/nvim/" = {
-      source = ./nvim;
-      recursive = true;
-    };
+      ".tmux.conf".source = ./.tmux.conf;
+      ".config/fuzzel/".source = ./fuzzel;
+      ".config/alacritty/".source = ./alacritty;
+      ".config/nvim/" = {
+        source = ./nvim;
+        recursive = true;
+      };
 
-    file.".config/mako/".source = ./mako;
-    file.".config/fastfetch/".source = ./fastfetch;
-    file.".config/mpv/".source = ./mpv;
+      ".config/mako/".source = ./mako;
+      ".config/fastfetch/".source = ./fastfetch;
+      ".config/mpv/".source = ./mpv;
+    };
 
     # should not be modified i think
     stateVersion = "24.05";
   };
 
   services.mako.enable = true;
-
-  programs.home-manager.enable = true;
 }
