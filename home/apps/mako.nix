485da5397281ddd64c16c833c220ce81276ea50d
ech <pnam2311@gmail.com>
2025-05-11 13:50:53 +0700

    mako config

--- home/apps/mako.nix
+++ home/apps/mako.nix
@@ -4,14 +4,14 @@
     enable = true;
     settings = {
       font = "JetBrainsMono Nerd Font 12";
-      backgroundColor = "#${config.colorScheme.palette.base00}";
-      textColor = "#${config.colorScheme.palette.base05}";
-      borderColor = "#${config.colorScheme.palette.base0D}";
-      borderSize = 2;
-      borderRadius = 20;
+      background-color = "#${config.colorScheme.palette.base00}";
+      text-color = "#${config.colorScheme.palette.base05}";
+      border-color = "#${config.colorScheme.palette.base0D}";
+      border-size = 2;
+      border-radius = 20;
       padding = "10";
-      defaultTimeout = 3000;
-      maxVisible = 3;
+      default-timeout = 3000;
+      max-visible = 3;
       width = 400;
       height = 100;
     };
