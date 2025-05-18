e48a8c4f24a9bd21df64b45d3f9a4b4298178286
ech <pnam2311@gmail.com>
2025-05-18 23:50:11 +0700

    spaceduck

---
+++ home/colors/spaceduck.nix
@@ -0,0 +1,23 @@
+{
+  slug = "Spaceduck";
+  name = "spaceduck";
+  author = "me but i copied that sh";
+  palette = {
+    base00 = "0f111b"; # Default Background
+    base01 = "686f9a"; # lighter background (highlight)
+    base02 = "1b1c36"; # Selection Background
+    base03 = "30365f"; # Comments, Invisibles, Line Highlighting
+    base04 = "636D83"; # Dark Foreground (gutter, caret)
+    base05 = "ecf0c1"; # Default Foreground, Light text
+    base06 = "E5E9F0"; # Light Foreground (rarely used)
+    base07 = "ECEFF4"; # Brightest Foreground (rarely used)
+    base08 = "e33400"; # Variables, Red
+    base09 = "e39400"; # Integers, Orange
+    base0A = "e2b86b"; # Classes, Yellow
+    base0B = "8ebd6b"; # Strings, Green
+    base0C = "00a3cc"; # Escape chars, Cyan
+    base0D = "ce6f8f"; # Functions, Blue
+    base0E = "b3a1e6"; # Keywords, Purple
+    base0F = "BE5046"; # Deprecated, Brown/Red
+  };
+}
